2de5718c244e5349891095f4475c470c39d91215
Olof Olsson <olssonolof@hotmail.com>
2024-02-22 21:54:00 +0100

    create mac build script

---
+++ mac-build-script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# make entr watch for changes in the current directory and subdirectories
+
+find . -type f | entr -c sh -c 'make sofle:odon && cp sofle_rev1_odon.hex ~/keyboard/keyboard.hex'
