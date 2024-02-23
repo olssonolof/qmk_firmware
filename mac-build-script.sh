0cdcd795ee461e2491af109e1adc9af6369dad55
Olof Olsson <olssonolof@hotmail.com>
2024-02-23 11:34:33 +0100

    Keymaps for mac updated

--- mac-build-script.sh
+++ mac-build-script.sh
@@ -2,4 +2,6 @@
 
 # make entr watch for changes in the current directory and subdirectories
 
-find . -type f | entr -c sh -c 'make sofle:odon-mac && cp sofle_rev1_odon.hex ~/keyboard/keyboard.hex'
+#find . -type f | entr -c sh -c 'make sofle:odon-mac && cp sofle_rev1_odon.hex ~/keyboard/keyboard.hex'
+#find . -type f | entr -c sh -c 'make sofle:odon-mac && cp ./.build/sofle_rev1_odon-mac.hex ~/keyboard/keyboard.hex'
+make sofle:odon-mac && cp sofle_rev1_odon-mac.hex ~/keyboard/keyboard.hex
